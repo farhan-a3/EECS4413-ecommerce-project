30f3a2be248a76b97a1326456dbb2a61b1dbb57e
Farhan <farhan.a3hd@gmail.com>
2025-12-05 15:10:26 -0500

    move router and routes into App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,47 @@
-import { Home } from './pages'
 import './App.css'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import ScrollToTop from './components/ScrollToTop'
+import { Toaster } from 'react-hot-toast'
+
+import {
+  Home,
+  Product,
+  Products,
+  AboutPage,
+  ContactPage,
+  Cart,
+  Login,
+  Register,
+  Checkout,
+  PageNotFound,
+} from './pages'
+
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <ScrollToTop>
+        <Provider store={store}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product" element={<Products />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="*" element={<PageNotFound />} />
+            <Route path="/product/*" element={<PageNotFound />} />
+          </Routes>
+        </Provider>
+      </ScrollToTop>
+      <Toaster />
+    </BrowserRouter>
+  )
 }
 
 export default App
