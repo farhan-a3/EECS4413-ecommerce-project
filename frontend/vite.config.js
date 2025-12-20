f8eba8c1757757d6f7d31aa3605ade5c1dfbf08b
Abel Payyanickal <sabuabel2003@gmail.com>
2025-12-20 18:22:46 -0500

    as

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Optional: set to 3000 if you prefer
+    host: true,
+    port: 5173, // Optional: set to 3000 if you prefer
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:8080',
         changeOrigin: true,
       }
     }
