b39de9b753441d83762605d58a94f374cb7905ed
Farhan <farhan.a3hd@gmail.com>
2025-12-20 22:56:17 -0500

    docker fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const proxyTarget = process.env.VITE_PROXY_TARGET || "http://localhost:5000";
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000, // Optional: set to 3000 if you prefer
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: proxyTarget,
         changeOrigin: true,
-      }
+        secure: false,
+      },
     }
   }
 })
